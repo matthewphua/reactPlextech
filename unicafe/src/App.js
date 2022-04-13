72280a2accf6d30723869990fec1438aee59f0da
Matthew Phua <matthew.phua@icloud.com>
2022-04-12 21:40:22 -0700

    adding

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+// a proper place to define a component
 
-function App() {
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+const Statistics = (props) => {
+  if (props.feedback) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <text>good {props.good}</text>
+        <br></br>
+        <text>neutral {props.neutral}</text>
+        <br></br>
+        <text>bad {props.bad}</text>
+      </div>
+    )
+  } else {
+
+    return (
+      <div>
+        <h1>statistics</h1>
+        <text>No Feedback Given!</text>
+      </div>
+    )
+  }
+}
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [feedback,setFeedback] = useState(false)
+  const handleGoodButtonClick = () => {
+    console.log(feedback)
+    setFeedback(true);
+    setGood(good+1);
+  }
+  const handleBadButtonClick = () => {
+    setFeedback(true);
+    setBad(bad+1);
+  }
+  const handleNeutralButtonClick = () => {
+    setFeedback(true);
+    setNeutral(neutral+1);
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodButtonClick} text="good"></Button> 
+      <Button handleClick={handleNeutralButtonClick} text="neutral"></Button> 
+      <Button handleClick={handleBadButtonClick} text="bad"></Button> 
+
+      <Statistics feedback={feedback} good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
-  );
+  )
 }
 
 export default App;
